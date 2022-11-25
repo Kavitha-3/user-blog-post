367aa498e3743e5c73a76e6fbed5b32b89de1718
Kavitha-3 <88623436+Kavitha-3@users.noreply.github.com>
2022-11-25 23:08:25 +0530

    Update user.routes.js

--- users/user.routes.js
+++ users/user.routes.js
@@ -9,7 +9,7 @@ const {userController}=require("./user.controller");
 const userRoute=Router();
 
 userRoute.get('/getusers',Authorization,userController.getAll);
-userRoute.get('/me',Authorization,userController.getMyData);
+userRoute.get('/:id',Authorization,userController.getMyData);
 userRoute.post('/register',userController.register);
 userRoute.post('/login',userController.login);
 
